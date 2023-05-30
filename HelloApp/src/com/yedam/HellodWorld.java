c35fee9d695f028c896b2a51b75e2c4bba453960
admin <admin@DESKTOP-UE9HA79>
2023-05-30 10:43:18 +0900

    first commit.

---
+++ HelloApp/src/com/yedam/HellodWorld.java
@@ -0,0 +1,9 @@
+package com.yedam;
+
+public class HellodWorld {
+	//main 메소드 시작.
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+	    System.out.println("git 연동.");
+	}
+}
---
+++ HelloApp/src/com/yedam/NewWorld.java
@@ -0,0 +1,9 @@
+package com.yedam;
+
+public class NewWorld {
+	public static void main(String[] agrs){
+	System.out.println("New World");
+    System.out.println("git 연동.");
+	}
+}
+
